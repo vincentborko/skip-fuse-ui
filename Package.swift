80b2cfb0b44394e3c2aec9da7acb0aa11ae2ae58
vborko <vincent.borko@gmail.com>
2026-01-25 03:59:59 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://source.skip.tools/skip-bridge.git", "0.16.4"..<"2.0.0"),
         .package(url: "https://source.skip.tools/skip-android-bridge.git", "0.6.1"..<"2.0.0"),
         .package(url: "https://source.skip.tools/swift-jni.git", "0.3.1"..<"2.0.0"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.46.0") 
+        .package(path: "../skip-ui") 
     ],
     targets: [
         .target(name: "SkipFuseUI", dependencies: ["SkipSwiftUI"]),
